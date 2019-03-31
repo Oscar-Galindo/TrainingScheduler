2ab4d7f7cd45c61677fa9581358fca51389243ae
Oscar-Galindo <46630550+Oscar-Galindo@users.noreply.github.com>
2019-03-31 16:56:58 -0400

    Update app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { NgModule } from '@angular/core';
 import { LoginComponent } from './common/auth/login.component';
+import { SignUpComponent } from './common/auth/sign-up.component'; //HOMEWORK Session 2
 
 const appRoutes: Routes = [
   { path: 'home', component: HomeComponent },
   { path: 'login', component: LoginComponent },
+  { path: 'sign-up', component: SignUpComponent }, //HOMEWORK Session 2
   { path: '**', component: HomeComponent },
 ];
 

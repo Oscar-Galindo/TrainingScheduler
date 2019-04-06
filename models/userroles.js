b93a5c9607d880c3c920b15b51153b3b513e7e57
Oscar Galindo <OGalindo@milestraining.lab>
2019-04-06 15:46:29 -0400

    Added FK

--- models/userroles.js
+++ models/userroles.js
@@ -1,16 +1,14 @@
 'use strict';
 module.exports = (sequelize, DataTypes) => {
   const UserRoles = sequelize.define('UserRoles', {
-    id: {
-      allowNull:false,
-      autoIncrement:true,
-      primaryKey: true,
-      type: DataTypes.INTEGER,
-    },
-    role: DataTypes.STRING
-  }, {});
-  UserRoles.associate = function(models) {
-    // associations can be defined here
-  };
+    
+  role: DataTypes.STRING
+  }, {
+    classMethods:{
+      associate = function(models) {
+        // associations can be defined here
+      }
+    }
+  });
   return UserRoles;
 };
